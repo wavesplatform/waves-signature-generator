46b8b00ae17e8854402b9649e3554842c56e05c8
tsigel <tsddaniil@gmail.com>
2018-05-28 12:55:42 +0300

    change export libs format

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,20 @@ export * from './config/Config';
 export * from './config/interface';
 export * from './signatureFactory/interface';
 export * from './signatureFactory/SignatureFactory';
-export * from './utils/concat';
-export * from './utils/crypto';
-export * from './utils/convert';
-export * from './libs/base58';
+import * as concatModule from './utils/concat';
+import * as cryptoModule from './utils/crypto';
+import * as convertModule from './utils/convert';
+import * as base58Module from './libs/base58';
+
+export const concat = {
+    ...concatModule
+};
+export const crypto = {
+    ...cryptoModule
+};
+export const convert = {
+    ...convertModule
+};
+export const base58 = {
+    ...base58Module
+};
