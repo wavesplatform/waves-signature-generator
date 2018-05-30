e9689fe466207ce96143812179c7453d8c8a84f9
tsigel <tsddaniil@gmail.com>
2018-05-30 18:03:51 +0300

    change export format

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,19 @@ export * from './config/Config';
 export * from './config/interface';
 export * from './signatureFactory/interface';
 export * from './signatureFactory/SignatureFactory';
-import * as concatModule from './utils/concat';
-import * as cryptoModule from './utils/crypto';
-import * as convertModule from './libs/converters';
-import * as base58Module from './libs/base58';
+import base58 from './libs/base58';
+import converters from './libs/converters';
+import { concatUint8Arrays } from './utils/concat';
+import convert from './utils/convert';
+import crypto from './utils/crypto';
 
-export const concat = {
-    ...concatModule
+export const libs = {
+    base58,
+    converters
 };
-export const crypto = {
-    ...cryptoModule
-};
-export const convert = {
-    ...convertModule
-};
-export const base58 = {
-    ...base58Module
+
+export const utils = {
+    concatUint8Arrays,
+    convert,
+    crypto
 };
